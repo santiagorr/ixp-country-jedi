7688ac697e37a40e00d88634ccd7336956d8eeb0
slowr <mavromat@ics.forth.gr>
2016-05-23 07:48:35 -0700

    visualize multiple IXP crossings

--- example/UG/analysis/common/jedi-utils.js
+++ example/UG/analysis/common/jedi-utils.js
@@ -1,15 +1,12 @@
 // .format() for strings
 // First, checks if it isn't implemented yet.
 if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined' ? args[number] : match;
+        });
+    };
 }
 
 // requires files in the right place
@@ -17,12 +14,20 @@ if (!String.prototype.format) {
 // args:
 //   d: d3 data elt
 //   jquery_elt: a jquery_elt to put the resulting html at
-function jedi_cell_detail_to_jquery_elt( proto, src_id , dst_id , jquery_elt ) {
-   json_file = "../common/details/{0}/{1}/{2}/latest.json".format( proto, src_id, dst_id );
-   $.ajax({url: json_file, 
-      async: true
-   }).done(function( data ) {
-      var txt = '<pre>{0}</pre>'.format( data['tracetxt'] );
-      jquery_elt.html( txt );
-   });
+function jedi_cell_detail_to_jquery_elt(proto, src_id, dst_id, jquery_elt) {
+    json_file = "../common/details/{0}/{1}/{2}/latest.json".format(proto, src_id, dst_id);
+    $.ajax({
+        url: json_file,
+        async: true
+    }).done(function(data) {
+        var txt = '<pre>{0}</pre>'.format(data['tracetxt']);
+        if (typeof(data['traixroute']) !== 'undefined') {
+            for (var k = 0; k < data['traixroute'].length; k++) {
+                txt += '<pre>Name of IXP: <b>{0}</b></br>'.format(data['traixroute'][k]['name']);
+                txt += 'IXP on hop: <b>{0}</b></br>'.format(data['traixroute'][k]['hop']);
+                txt += 'IXP on the same country? <b>{0}</b></pre>'.format(data['traixroute'][k]['in_country']);
+            }
+        }
+        jquery_elt.html(txt);
+    });
 }
